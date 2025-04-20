a36349b389109a5bea1eaa6c002c0b20dbc5ddad
Koushik <banerjeekoushik04@gmail.com>
2025-04-21 02:13:43 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
 		<AppContext.Provider value={{ theme, switchTheme }}>
 			<BrowserRouter>
 				<Routes>
-					<Route path="/" element={<Homepage />} />
+					<Route path="/Portfolio-Website" element={<Homepage />} />
 					<Route path="*" element={<NotFound />} />
 				</Routes>
 			</BrowserRouter>
