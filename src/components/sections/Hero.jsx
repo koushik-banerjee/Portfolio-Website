15fb2c655f5855b8fa138df4467f4d24dcf393e1
Koushik <banerjeekoushik04@gmail.com>
2025-04-25 20:37:15 +0530

    commit changes

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -6,28 +6,21 @@ function Hero() {
   return (
     <section
       id="hero"
-      className="pb-28 pt-24 sm:pt-28 md:pt-24 flex px-6 lg:px-24"
+      className="pb-28 pt-16 md:pt-20 lg:pt-24 flex px-6 lg:px-24"
     >
       <div className="self-center">
         {/* =========== MOBILE IMAGE + TOOLTIP =========== */}
-        <div className="hs-tooltip [--placement:right] w-20 hs-tooltip-toggle">
-          <img
-            src={user_info.main.photo}
-            className="rounded-full mb-6 lg:hidden"
-            alt="Koushik Banerjee"
-          />
-          <span
-            className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm"
-            role="tooltip"
-          >
-            Hello! 👋 How are you doing? 🤔
-          </span>
-        </div>
+        <div className="hs-tooltip [--placement:right] w-40 md:w-52 hs-tooltip-toggle mx-auto flex justify-center">
+  <img src={user_info.main.photo} className="rounded-full mb-6 lg:hidden" alt="Koushik Banerjee" />
+  <span className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm" role="tooltip">
+    Hello! 👋 How are you doing? 🤔
+  </span>
+</div>
 
         {/* =========== MAIN TEXT CONTENT =========== */}
         <div className="flex gap-2 align-center flex-wrap md:flex-nowrap">
           <div className="lg:w-[80%] text-zinc-900 dark:text-zinc-100 self-center">
-            <h2 className="text-xl font-semibold text-red-700 dark:text-red-500">
+            <h2 className="text-xl font-semibold text-red-700 dark:text-red-500 text-center lg:text-start">
               <Typewriter
                 words={["Full Stack Developer", "Frontend Developer", "Backend Developer"]}
                 loop={0}
@@ -38,15 +31,15 @@ function Hero() {
                 delaySpeed={1000}
               />
             </h2>
-            <h1 className="font-black mt-3 text-5xl lg:w-[85%]">
+            <h1 className="font-black mt-3 text-4xl md:text-5xl lg:w-[85%] text-center lg:text-left">
               {user_info.main.name}
             </h1>
 
-            <p className="mt-6 dark:text-zinc-300 text-base font-light lg:w-[87%] leading-7">
+            <p className="mt-6 dark:text-zinc-300 text-base font-light lg:w-[87%] leading-7 text-center lg:text-start">
               {user_info.main.description}
             </p>
 
-            <div className="flex gap-2 mt-6">
+            <div className="flex gap-2 mt-6 items-center justify-center lg:items-start lg:justify-start">
               <a
                 href="#projects"
                 className="px-6 py-3 border border-black relative overflow-hidden group dark:border-white font-medium transition-all duration-300 text-black dark:text-white"
